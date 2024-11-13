1e24482aa3f3059ac2a4ce3581afb9f840333f57
trhoaq <hoangphi.wordcite.zaun@gmail.com>
2024-11-13 18:27:39 +0700

    update oop

--- oop.py
+++ oop.py
@@ -1,15 +1,16 @@
+import curses
+
 class task:
-    def __init__(self, name, date_time, note=None, priority=None ): #init programme has name, was completed or no, date and priority
+    def __init__(self, name, date_time, note=None, priority=None ): 
         self.name = name 
         self.completed = False
         self.day = date_time
         self.place = priority
         self.note = note
 
-    def __str__(self): #if task completed, programme will print [x] before else print [ ] 
+    def __str__(self): 
         return f"{"[x]" if self.completed else [ ]} {self.name} (priority = {self.place}, day = {self.day}, note = {self.note}"
     
-    #mark cpled or not
     def mark_completed(self): 
         self.completed = True
 
@@ -28,10 +29,16 @@ class task:
         for task in self.name:
             if task == name:
                 self.note += adding_note
-    
 
+    def update_day(self, name, new_date):
+        for task in self.name:
+            if task == name:
+                self.day = new_date
 
-
+    def new_priority(self, name, priority):
+        for task in self.name:
+            if task == name:
+                self.place = priority
 
 class todo:
     def __init__(self):
@@ -40,14 +47,25 @@ class todo:
     def new_task(self, new_task):
         self.task.append(new_task)
 
-    def add_date(self, date_time):
-        task.day = date_time
-
     def remove_task(self, old_task):
         for task in self.task:
             if old_task == task:
                 self.task.remove(task)
+
+    def add_date(self, date_time):
+        task.day = date_time
+
+    def change_date(self, name, new_date):
+        for task in self.task:
+            if task == name:
+                task.day = new_date
+        
+    def add_note(self, note):
+        task.note = note
     
+    def more_note(self, name, note):
+        task.update_note(name, note)
+
     def view_task(self):
         if self.task == []:
             print("no task")
@@ -55,7 +73,7 @@ class todo:
             for task in self.task:
                 print(task)
 
-    def check_note(self, name):
+    def view_note(self, name):
         for task in self.task:
             if task == name:
                 print("note: ", f"{task.note}")
@@ -82,25 +100,21 @@ def main():
     print("\n1. new task")
     print("2. view task")
     print("3. remove task")
-    print("4. check note")
+    print("4. edit date")
     print("5. edit note")
+    print("6.edit priority")
     
     choice = int(input(""))
 
     if choice == 1:
         name = input("task name: ")
-        task1 = todo.new_task(task(name))
-        print(task1)
+        date = input("date: ")
+        note = input("add note to task: ")
+        priority = int(input("priority: "))
 
 
 
+    
 
-
-
-
-
-
-
-
-
-
+        
+            
