ff523a3143fb8cc10779538d80b40194473f501c
trhoaq <hoangphi.wordcite.zaun@gmail.com>
2024-11-14 17:23:08 +0700

    opp

--- oop.py
+++ oop.py
@@ -9,7 +9,7 @@ class task:
         self.note = note
 
     def __str__(self): 
-        return f"{"[x]" if self.completed else [ ]} {self.name} (priority = {self.place}, day = {self.day}, note = {self.note}"
+        return f"{"[x]" if self.completed else [ ]} {self.name} (priority = {self.place}, day = {self.day}, note = {self.note})"
     
     def mark_completed(self): 
         self.completed = True
@@ -20,29 +20,18 @@ class task:
     def update_name(self, new_name):
         self.name = new_name
     
-    def new_note(self, name, new_note):
-        for task in self.name:
-            if task == name:
-                self.note = new_note
+    def new_note(self, new_note):
+        self.note = new_note
 
-    def update_note(self, name, adding_note):
-        for task in self.name:
-            if task == name:
-                self.note += adding_note
-
-    def update_day(self, name, new_date):
-        for task in self.name:
-            if task == name:
-                self.day = new_date
-
-    def new_priority(self, name, priority):
-        for task in self.name:
-            if task == name:
-                self.place = priority
+    def new_priority(self, priority):
+        self.place = priority
 
 class todo:
     def __init__(self):
         self.task = []
+
+    def list_task(self):
+        return self.task()
 
     def new_task(self, new_task):
         self.task.append(new_task)
@@ -52,20 +41,21 @@ class todo:
             if old_task == task:
                 self.task.remove(task)
 
-    def add_date(self, date_time):
-        task.day = date_time
-
     def change_date(self, name, new_date):
         for task in self.task:
             if task == name:
                 task.day = new_date
         
-    def add_note(self, note):
-        task.note = note
-    
-    def more_note(self, name, note):
-        task.update_note(name, note)
+    def new_note(self, name, new_note):
+        for task in self.task:
+            if task == name:
+                task.day = new_note
 
+    def add_note(self, name, add_note):
+        for task in self.task:
+            if task == name:
+                task.note += add_note
+    
     def view_task(self):
         if self.task == []:
             print("no task")
@@ -76,7 +66,8 @@ class todo:
     def view_note(self, name):
         for task in self.task:
             if task == name:
-                print("note: ", f"{task.note}")
+                print("note: ", task.note )
+                return
             else:
                 print("task not found")
         
@@ -86,31 +77,70 @@ class todo:
                 task.update_name(new_name)
 
     def sort_priority(self):
-        self.task.sort(key=lambda x: x.priority if x.priority else 0)
+        task.priority.sort(key= lambda x: x.place if x.place else 0)
     
-    def list_of_task_was_completed(self):
-        self.list_of_task_was_completed = []
+    def list_completed(self):
+        self.list_completed = []
         for task in self.task: 
-            self.list_of_task_was_completed.append(task)
+            if task.completed:
+                self.list_completed.append(task) 
+
+class pointer:
+    def __init__(self, stdscr):
+        self.stdscr = stdscr
+        self.stdscr.clear()
+        curses.curs_set(0)
+
+    def display(self):
+        self.stdscr.clear()
+        self.stdscr.addstr(0 ,0 ,"1. new task")
+        self.stdscr.addstr(1 ,0 ,"2. view task")
+        self.stdscr.addstr(2 ,0 ,"3. remove task")
+        self.stdscr.addstr(3 ,0 ,"4. edit date")
+        self.stdscr.addstr(4 ,0 ,"5. edit note")
+        self.stdscr.addstr(5 ,0 ,"6.edit priority")
 
 
+        for i, task in enumerate(todo.task):
+            self.stdscr.addstr(i + 1, 0, str(task))
+        
+        self.stdscr.refresh()
 
-def main():
+    def handle_input(self):
+        key = self.stdscr.getch()
+
+        if key == ord('q'): 
+            return False
+        
+        elif key == ord('m'): 
+            task.mark_completed()
+
+        elif key == ord('r'):  
+            todo.remove_task()
+
+        elif key == ord('1'):
+            self.stdscr.clear()
+            self.stdscr.addstr( 1, 0, "name task: ")
+            curses.echo()
+
+            name = self.stdscr.getstr( 02, 0, 60).decode("utf-8").strip()
+            if name :
+                todo.new_task.append(name)
+            
+            self.stdscr.refresh()
+            self.stdscr.getch()
+
+        elif key == ord('2'):
+            self.stdscr.clear()
+            self.stdscr.addstr( 0, 0, str(todo.view_task))
+
+        elif key == ord('3'):
+            if len(todo.list_task()) > 0:
+                
+                curses.echo()
+                del_task = self.stdscr.getstr()
+
     
-    print("\n1. new task")
-    print("2. view task")
-    print("3. remove task")
-    print("4. edit date")
-    print("5. edit note")
-    print("6.edit priority")
-    
-    choice = int(input(""))
-
-    if choice == 1:
-        name = input("task name: ")
-        date = input("date: ")
-        note = input("add note to task: ")
-        priority = int(input("priority: "))
 
 
 
